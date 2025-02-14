20c99e3ce9a31a7fc1512419154b5762b96ae780
Ivan Mysovskii <vano641@mail.ru>
2025-02-14 10:49:33 +0300

    main - содержит метод запуска программы

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.io.FileNotFoundException;
+
+import view.viewRun;
+
+public class main {
+    public static void main(String[] args) throws FileNotFoundException {
+        viewRun run = new viewRun();
+        run.run();
+
+    }
+}
